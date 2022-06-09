e2cdd5c503e8e239054c6386a27e356e7b526586
Partha Bora <Partha.Bora@marlabs.com>
2022-06-09 19:33:40 +0530

    2 commit

--- filesystem.js
+++ filesystem.js
@@ -1,9 +1,33 @@
 let fs = require('fs')
 
 //sync
-let data = fs.readFileSync('readMe.txt')
-console.log(data)
+// try{
+//     let data = fs.readFileSync('readMe.txt', 'utf-8')
+//     console.log(data)
+//     fs.writeFileSync('writeMe.txt', data)
+// }
+// catch(er){
+//     console.log(er.message)
+// }
 
 
 
 //async
+//try catch --> callbacks
+
+fs.readFile('readMe.txt', 'utf-8', (err, data)=>{
+    if(err === null){
+        console.log(data)
+        fs.writeFile('writeMe.txt', data, (err)=>{
+            if(err === null){
+                console.log('File written successfully')
+            }
+            else{
+                console.log(err.message)
+            }
+        })
+    }
+    else{
+        console.log(err.message)
+    }
+})
