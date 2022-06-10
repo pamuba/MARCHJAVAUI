b2a4e852c2befc3d0a27d67d787c7b098cc64040
Partha Bora <Partha.Bora@marlabs.com>
2022-06-10 19:00:58 +0530

    2 commit

---
+++ eventsdemo.js
@@ -0,0 +1,30 @@
+let events = require('events')
+
+// var emitter = new events.EventEmitter()
+
+// //click the button(event) - emit the event
+
+// emitter.on('click', function(msg){
+//     console.log(msg)
+// })
+
+// //similar to click
+// emitter.emit('click', 'The event is raised/emitted')
+
+//import for inheritance
+// var util = require('util')
+
+// var Person = function(name){
+//     this.name = name
+// }
+
+// util.inherits(Person, events.EventEmitter)
+
+// var james = new Person('James')
+
+
+// james.on('speak', function(){
+//     console.log("Helllooooo!!!")
+// })
+
+// james.emit('speak')
--- filesystem.js
+++ filesystem.js
@@ -15,19 +15,54 @@ let fs = require('fs')
 //async
 //try catch --> callbacks
 
-fs.readFile('readMe.txt', 'utf-8', (err, data)=>{
-    if(err === null){
-        console.log(data)
-        fs.writeFile('writeMe.txt', data, (err)=>{
-            if(err === null){
-                console.log('File written successfully')
-            }
-            else{
-                console.log(err.message)
-            }
-        })
-    }
-    else{
-        console.log(err.message)
-    }
-})
+// fs.readFile('readMe.txt', 'utf-8', (err, data)=>{
+//     if(err === null){
+//         console.log(data)
+//         fs.writeFile('writeMe.txt', data, (err)=>{
+//             if(err === null){
+//                 console.log('File written successfully')
+//             }
+//             else{
+//                 console.log(err.message)
+//             }
+//         })
+//     }
+//     else{
+//         console.log(err.message)
+//     }
+// })
+
+
+//deleting a file
+// fs.unlink('writeMe.txt', (err)=>{
+//     if(err != null){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File Deleted Successfully")
+//     }
+// })
+
+
+//creating a folder
+// fs.mkdir('folder', (err)=>{
+//     if(err != null){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("Folder Created Successfully")
+//     }
+// })
+
+//deleting a folder
+// fs.rmdir('folder', (err)=>{
+//     if(err != null){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("Folder Deleted Successfully")
+//     }
+// })
+
+
+//WAP to delete a non empty folder
